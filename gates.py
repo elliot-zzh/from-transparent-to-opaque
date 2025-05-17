fix the problem of gradient explosion, add config of the layer num of the injected layer

gates.py
@@ -17,7 +17,7 @@ class CurrentStepMixerGater(nn.Module):
 
     def forward(self, hidden, embed):
         x = torch.cat([hidden, embed], dim=-1)
-        return self.act(self.w(x))
+        return self.act(self.w(x)) * 2
 
 
 class Gate(nn.Module):
@@ -28,15 +28,17 @@ class Gate(nn.Module):
         self.inject_scale = inject_scale
 
         self.norm = nn.RMSNorm(embed_dim)
-        self.time_mixing_gate = CurrentStepMixerGater(embed_dim)
+        self.injection_gate = CurrentStepMixerGater(embed_dim)
+        self.origin_gate = CurrentStepMixerGater(embed_dim)
 
     @torch.compile
     def forward(self, hidden, embed):
         hidden = self.norm(hidden)
-        gate = self.time_mixing_gate(hidden, embed) * 2
-        return embed * gate + (embed**2).mean() ** 0.5 * (1 - gate) * hidden
+        injection_gate = 1 - self.injection_gate(hidden, embed)
+        origin_gate = self.origin_gate(hidden, embed)
+        return embed * origin_gate + (embed**2).mean() ** 0.5 * injection_gate * hidden
 
     @torch.compile
     def forward_hidden(self, hidden, embed):  # forward hidden only
-        gate = self.time_mixing_gate(hidden, embed) * 2
-        return (embed**2).sum() ** 0.5 * (1 - gate) * hidden, gate
+        injection_gate = 1 - self.injection_gate(hidden, embed)
+        return (embed**2).mean() ** 0.5 * injection_gate * hidden, injection_gate

sampler.py
@@ -64,6 +64,9 @@ def sampler(
     res_probs = torch.zeros(problem_batch_size, 0, dtype=torch.float32).to(device)
 
     for i in tqdm(range(max_length), desc="sampling progress"):
+        if i % gc_interval == 0:
+            cleanup()
+
         with accelerator.autocast():
             hidden_cache = torch.cat(
                 [
@@ -84,36 +87,35 @@ def sampler(
                 for depth_i in range(depth):
                     last_hidden = model_forward(
                         hidden_state=last_hidden,
-                        attn_mask=attn_mask[:, input_ids.shape[1] + i - 1 :],
-                        pos=cache_pos[-1:] - input_ids.shape[1] + 1,
+                        attn_mask=attn_mask[
+                            :, input_ids.shape[1] + i - 1 :
+                        ].contiguous(),
+                        pos=(cache_pos[-1:] - input_ids.shape[1] + 1).contiguous(),
                         kv_cache=deep_kv_cache[depth_i],
                         start_layer=depth_start_layer_num,
                         end_layer=hidden_layer_num,
                     )
                 uncompressed_hidden = last_hidden
 
-        if i % gc_interval == 0:
-            cleanup()
-
-        probs = nn.functional.softmax(logits / temperature, dim=-1)
-        probs_ = nn.functional.softmax(
-            logits, dim=-1
-        )  # without temperature, for training
-        topk_probs, indices = torch.topk(
-            probs, topk, largest=True, sorted=False, dim=-1
-        )
-        probs_ = probs_.gather(1, indices)
-        # probs = probs.masked_fill(probs < min_p * 1 / topk, 0)
-        selected_choice = torch.multinomial(
-            topk_probs.view(problem_batch_size, -1), num_samples=1
-        )
-        selected_index = indices.gather(1, selected_choice)
-        selected_probs = probs_.gather(1, selected_choice)
-        selected_index[(1 - text_end_mask).bool(), :] = eot
-        selected_probs[(1 - text_end_mask).bool(), :] = 1e6  # mask out
-        res = torch.cat([res, selected_index], dim=1)
-        res_probs = torch.cat([res_probs, selected_probs], dim=1)
-        selected_index = selected_index.view(problem_batch_size)
+            probs = nn.functional.softmax(logits / temperature, dim=-1)
+            probs_ = nn.functional.log_softmax(
+                logits, dim=-1
+            )  # without temperature, for training
+            topk_probs, indices = torch.topk(
+                probs, topk, largest=True, sorted=False, dim=-1
+            )
+            probs_ = probs_.gather(1, indices)
+            # probs = probs.masked_fill(probs < min_p * 1 / topk, 0)
+            selected_choice = torch.multinomial(
+                topk_probs.view(problem_batch_size, -1), num_samples=1
+            )
+            selected_index = indices.gather(1, selected_choice)
+            selected_probs = probs_.gather(1, selected_choice)
+            selected_index[(1 - text_end_mask).bool(), :] = eot
+            selected_probs[(1 - text_end_mask).bool(), :] = 1e6  # mask out
+            res = torch.cat([res, selected_index], dim=1)
+            res_probs = torch.cat([res_probs, selected_probs], dim=1)
+            selected_index = selected_index.view(problem_batch_size)
 
         if not gen_all_done and eot in selected_index:
             # text_end_appeared = True
@@ -136,12 +138,12 @@ def sampler(
             embeds = model.model.model.embed_tokens(
                 selected_index.view(problem_batch_size, 1)
             )
-            embeds = gater(uncompressed_hidden, embeds)
             final_hidden, last_hidden = model_forward(
                 hidden_state=embeds,
                 attn_mask=attn_mask,
                 pos=cache_pos,
                 kv_cache=kv_cache,
+                hidden_injection=uncompressed_hidden,
                 extract_specific=hidden_layer_num,
             )
             logits = model.lm_head(model.model.model.norm(final_hidden[:, -1, :]))

train.py
@@ -94,6 +94,7 @@ def norm(x: torch.Tensor) -> torch.Tensor:
 
 linear_interpl = torch.jit.script(linear_interpl)
 norm = torch.jit.script(norm)
+lossf = torch.nn.CrossEntropyLoss(reduction="none")
 
 
 def train():
@@ -234,7 +235,7 @@ def train():
                             if i + batch_size <= res.shape[0]
                             else res.shape[0]
                         )
-                        embeds = model.lm_head.weight[seqs[i:end]][:, :-1].to("cuda:0")
+                        embeds = model.lm_head.weight[seqs[i:end]][:, :-1]
                         hidden_cache_slice = hidden_cache[i:end]
                         with accelerator.autocast():
                             last_hidden = vae.uncompress(
@@ -279,10 +280,20 @@ def train():
                                     seqs.shape[1] - 1, dtype=torch.long, device=device
                                 ),
                                 extract_specific=hidden_layer_num,
+                                hidden_injection=last_hidden,
+                                hidden_injecting_func=lambda hidden_injection,
+                                hidden_state: torch.cat(
+                                    [
+                                        hidden_state[:, : input_ids.shape[1]],
+                                        gater(
+                                            last_hidden,
+                                            hidden_state[:, input_ids.shape[1] :],
+                                        ),
+                                    ],
+                                    dim=1,
+                                ),
                             )
-                            logits = model.lm_head(
-                                model.model.model.norm(final_hidden)
-                            ).float()
+                            logits = model.lm_head(model.model.model.norm(final_hidden))
 
                             """
                             loss = lossf(
@@ -294,22 +305,23 @@ def train():
                             """
 
                             # compute loss
-                            target = res[i:end, 1:]
+                            target = res[i:end]
                             target[target >= logits.shape[-1]] = 0
                             new_probs = (
-                                F.softmax(logits[:, input_ids.shape[1] - 1 :], dim=-1)
+                                F.log_softmax(
+                                    logits[:, input_ids.shape[1] - 1 :], dim=-1
+                                )
                                 .gather(-1, target.unsqueeze(-1))
                                 .squeeze(-1)
                             )
-                            clipped = loss = new_probs / res_probs[i:end, 1:]
-                            clipped[clipped >= clip_high + 1] = 1 + clip_high
-                            clipped[clipped <= 1 - clip_low] = 1 - clip_low
+                            loss = torch.exp(new_probs - res_probs[i:end])
+                            clipped = torch.clamp(loss, 1 - clip_high, 1 + clip_low)
                             clipped *= rewards[i:end].unsqueeze(-1)
                             loss = torch.min(
                                 loss * rewards[i:end].unsqueeze(-1), clipped
                             )
-                            loss *= mask[i:end, input_ids.shape[1] + 1 :]
-                            loss = (
+                            loss *= mask[i:end, input_ids.shape[1] :]
+                            dapo_loss = loss = (
                                 (loss.sum(dim=-1))
                                 / (text_end_indices[i:end] + 1).sum()
                                 * (
@@ -357,17 +369,23 @@ def train():
                                 1,
                                 hidden_reg_len_bonus_high,
                             ).unsqueeze(1)
+                            hidden_loss = (
+                                hidden_loss.mean() * hidden_regularization_rate
+                            )
+                            loss += hidden_loss
+
                             # apply gating value bonus
                             gate_bonus = torch.exp(
                                 gating_value_lambda * (0.5 - gate) ** 2
                             ).mean()
-                            loss += hidden_loss.mean() * hidden_regularization_rate
-                            loss += (
+
+                            gate_bonus = (
                                 gate_bonus
                                 * gating_value_bonus
                                 * gating_value_decay
                                 ** (step // gating_bonus_update_step)
                             )
+                            loss += gate_bonus
                             loss *= (end - i) / res.shape[0]
 
                             # randomly update hidden cache
@@ -387,6 +405,10 @@ def train():
 
                 print(f"Step {step}, Loss: {loss.item():.3f}")
                 print("gating values: ", gate[:1, :10])
+
+                print(
+                    f"hidden loss {hidden_loss}, gate bonus {gate_bonus} dapo_loss {dapo_loss}"
+                )
 
                 step += 1
 