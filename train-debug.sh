f55e69b434aa07f516815268aa8816e70d463cb9
zzh <elliot_zh@proton.me>
2025-06-18 16:36:24 +0800

    chore: change debug data file name

--- train-debug.sh
+++ train-debug.sh
@@ -1,3 +1,3 @@
 echo "Starting..."
-CUDA_VISIBLE_DEVICES=0 python train.py --config=config-debug.toml --traindataset=/home/featurize/data/open-r1-math-220k-256.jsonl
+CUDA_VISIBLE_DEVICES=0 python train.py --config=config-debug.toml --traindataset=/home/featurize/data/open-r1-math-220k.jsonl
 # python train.py
