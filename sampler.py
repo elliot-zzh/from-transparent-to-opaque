fix: use SinkCache to reduce memory usage, update hyperparameters, topk before softmax to reduce training vram usage

sampler.py
@@ -53,7 +53,7 @@ def sampler(
     # tokenize
     problem_batch_size = input_ids.shape[0]
     cache_pos = torch.arange(input_ids.shape[1], dtype=torch.int, device=device)
-    kv_cache = DynamicCache()
+    kv_cache = SinkCache(num_sink_tokens=512, window_length=4096)
 
     # prefill the problem
     with accelerator.autocast():
@@ -91,15 +91,19 @@ def sampler(
         sample_probs = F.softmax(logits / temperature, dim=-1)
         sample_probs, topk_indices = torch.topk(
             sample_probs,
-            topk if enable_swapping else 1,
+            topk,
             largest=True,
             sorted=False,
             dim=-1,
         )
 
         # sampling the token
-        selected_choice = torch.multinomial(
-            sample_probs.view(problem_batch_size, -1), num_samples=1
+        selected_choice = (
+            torch.multinomial(sample_probs.view(problem_batch_size, -1), num_samples=1)
+            if enable_swapping
+            else torch.argmax(
+                sample_probs.view(problem_batch_size, -1), dim=-1
+            ).unsqueeze(-1)
         )
         selected_index = topk_indices.view(problem_batch_size, -1).gather(
             1, selected_choice
@@ -116,7 +120,12 @@ def sampler(
 
         # for concept token
         concept_mask = torch.cat(
-            [concept_mask, (selected_index == eoth).unsqueeze(-1)], dim=-1
+            [
+                concept_mask,
+                (1 - (selected_index == eoth).unsqueeze(-1).int())
+                * (concept_mask[:, -1:] if i > 0 else 1),
+            ],
+            dim=-1,
         )
         concept_probs = F.softmax(logits / concept_temperature, dim=-1)
         concept_probs = concept_probs.gather(-1, topk_indices)

train.py
@@ -80,8 +80,8 @@ def norm(x: torch.Tensor) -> torch.Tensor:
     return x / (x**2).mean() * 0.5
 
 
-def kl_divergence(p: torch.Tensor, q_logits: torch.Tensor, eps: float = 1e-10):
-    return (p * (torch.log(p + eps) - torch.log_softmax(q_logits, dim=-1))).sum(dim=-1)
+def kl_divergence(p: torch.Tensor, q: torch.Tensor, eps: float = 1e-10):
+    return (p * (torch.log(p + eps) - q)).sum(dim=-1)
 
 
 linear_interpl = torch.jit.script(linear_interpl)
@@ -304,25 +304,30 @@ def train():
                                 use_cache=False,
                                 output_hidden_states=False,
                                 return_dict=True,
-                            ).logits.float()
+                            ).logits
+                            shrunk_logits, shrunk_indices = torch.topk(
+                                logits[:, input_ids.shape[1] - 1 :],
+                                k=128,
+                                dim=-1,
+                                largest=True,
+                                sorted=False,
+                            )
+                            shrunk_logits = shrunk_logits.float()
 
                             # compute DAPO loss
-                            target = res[i:end, 1:]
+                            target = res[i:end, :]
                             target[target >= logits.shape[-1]] = 0
                             new_probs = (
-                                F.log_softmax(
-                                    logits[:, input_ids.shape[1] - 1 :], dim=-1
-                                )
-                                .gather(-1, target.unsqueeze(-1))
-                                .squeeze(-1)
-                            )
-                            loss = torch.exp(new_probs - res_probs[i:end, 1:])
+                                F.log_softmax(shrunk_logits, dim=-1)
+                                * (shrunk_indices == target.unsqueeze(-1)).float()
+                            ).sum(dim=-1)
+                            loss = torch.exp(new_probs - res_probs[i:end, :])
                             clipped = torch.clamp(loss, 1 - clip_high, 1 + clip_low)
                             clipped *= rewards[i:end].unsqueeze(-1)
                             loss = torch.min(
                                 loss * rewards[i:end].unsqueeze(-1), clipped
                             )
-                            loss *= mask[i:end, input_ids.shape[1] + 1 :]
+                            loss *= mask[i:end, input_ids.shape[1] :]
                             loss = (
                                 (loss.sum(dim=-1))
                                 / (text_end_indices[i:end] + 1).sum()
@@ -332,14 +337,19 @@ def train():
                             )
                             if enable_swapping:
                                 self_distillation_loss = kl_divergence(
-                                    concept_token_probs[i:end, 1:],
-                                    logits.gather(-1, concept_token_indices[i:end, :-1])
-                                    / concept_temperature,
+                                    concept_token_probs[i:end, :],
+                                    torch.log_softmax(
+                                        logits[:, input_ids.shape[1] - 1 :].gather(
+                                            -1, concept_token_indices[i:end, :]
+                                        )
+                                        / concept_temperature,
+                                        dim=-1,
+                                    ),
                                 )
-                                self_distillation_loss *= concept_mask[i:end, 1:]
+                                self_distillation_loss *= concept_mask[i:end, :]
                                 self_distillation_loss = (
                                     self_distillation_loss.sum(dim=-1)
-                                    / concept_mask[i:end, 1:].sum()
+                                    / concept_mask[i:end, :].sum()
                                 ) * rewards[i:end]
                                 self_distillation_loss = self_distillation_loss.sum()
                                 loss += (
@@ -361,9 +371,9 @@ def train():
 
                 print(rank, f'Step {step}, Loss: {loss.item():.8f}')
                 writer.add_scalar(
-                    'length/train', text_end_indices.mean().item() + 1, step
+                    'length/train', text_end_indices.float().mean().item() + 1, step
                 )
-                writer.add_text('sample_text/train', decoded, step)
+                writer.add_text('sample_text/train', decoded[0], step)
 
                 cleanup()
 