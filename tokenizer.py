ef0ac669817cf58f3e8ba0ac790f516aabb0257c
zzh <elliot_zh@proton.me>
2025-06-25 17:10:03 +0800

    ✨ feat: update prompt: "one single \\boxed{}"

--- tokenizer.py
+++ tokenizer.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer
 from config import model_name, model_path
 from parameters import enable_thinking
 
-prompt = '<|im_start|>user\n Solve the math problem below step by step, and eventually, **repeat your final answer in the LaTeX `\\boxed{}:`**\n. No need for multi-solution. No need to repeat the solving process in your final answering. Only the `\\boxed` wrapped result is required in your answer. Problem: '  # basic system prompt
+prompt = '<|im_start|>user\n Solve the math problem below step by step, and eventually, **repeat your final answer in one single LaTeX `\\boxed{}:`**\n. No need for multi-solution. No need to repeat the solving process in your final answering. Only one `\\boxed` wrapped result is required in your answer. Problem: '  # basic system prompt
 prompt_suffix = (
     '/think' if enable_thinking else '/no_think'
 ) + ' <|im_end|><|im_start|>assistant\n'
