fix: 🐛 remove logits shrinking and fix normalization missing in concept prob computation during training

train.py
@@ -305,6 +305,8 @@ def train():
                                 output_hidden_states=False,
                                 return_dict=True,
                             ).logits
+                            
+                            '''
                             shrunk_logits, shrunk_indices = torch.topk(
                                 logits[:, input_ids.shape[1] - 1 :],
                                 k=128,
@@ -313,14 +315,18 @@ def train():
                                 sorted=False,
                             )
                             shrunk_logits = shrunk_logits.float()
+                            '''
 
                             # compute DAPO loss
                             target = res[i:end, :]
                             target[target >= logits.shape[-1]] = 0
                             new_probs = (
-                                F.log_softmax(shrunk_logits, dim=-1)
-                                * (shrunk_indices == target.unsqueeze(-1)).float()
-                            ).sum(dim=-1)
+                                F.log_softmax(
+                                    logits[:, input_ids.shape[1] - 1 :], dim=-1
+                                )
+                                .gather(-1, target.unsqueeze(-1))
+                                .squeeze(-1)
+                            )
                             loss = torch.exp(new_probs - res_probs[i:end, :])
                             clipped = torch.clamp(loss, 1 - clip_high, 1 + clip_low)
                             clipped *= rewards[i:end].unsqueeze(-1)
@@ -336,13 +342,17 @@ def train():
                                 )  # here we want to maximaize it, aligned with DAPO target
                             )
                             if self_distillation_factor > 0:
+                                new_concept_probs = torch.log_softmax(
+                                    logits[:, input_ids.shape[1] - 1 :]
+                                    / concept_temperature,
+                                    dim=-1,
+                                ).gather(-1, concept_token_indices[i:end, :])
+                                new_concept_probs /= new_concept_probs.sum(
+                                    dim=-1, keepdim=True
+                                )
                                 self_distillation_loss = kl_divergence(
                                     concept_token_probs[i:end, :],
-                                    torch.log_softmax(
-                                        logits[:, input_ids.shape[1] - 1 :]
-                                        / concept_temperature,
-                                        dim=-1,
-                                    ).gather(-1, concept_token_indices[i:end, :]),
+                                    new_concept_probs,
                                 )
                                 self_distillation_loss *= concept_mask[i:end, :]
                                 self_distillation_loss = (